1981dc7abcad69f8c82c7e225f59c1a40f1dd4c5
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-09-14 19:09:57 -0300

    First attemps

---
+++ Primer ejercicio.R
@@ -0,0 +1,54 @@
+#Asigno variable, nombrando la variable y usando <- para asignar el valor
+
+NumeroVariable <- 3
+print(NumeroVariable)
+
+#El tipo de dato que asigno se define en la asignación.
+
+elmejordelmundo <- "Lionel Messi"
+print(elmejordelmundo)
+
+#Puedo operar entre numeros y variables facilmente
+
+1+1
+
+Sumador1 <- 10
+Sumador2 <- 5
+print(Sumador1+Sumador2)
+
+#Los conjuntos de datos se pueden crear con esta estructura:
+
+bolsaDeDatos <- c(5,6,7,8,9)
+print (bolsaDeDatos)
+plot(bolsaDeDatos)
+
+#Ejercicios
+#  ---------------------------------------
+# Crear un hola mundo
+# Crear una suma de dos variables en forma directa y con variables
+# Crear una resta de dos variables en forma directa y con variables
+# Calcular la potencia en forma directa y con variables
+# plotear un conjunto de datos
+
+mensaje <- "Hola Mundo"
+print(mensaje)
+2 + 2
+numeroA <- 7
+numeroB <- 8
+print(numeroA + numeroB)
+
+3 - 7
+numeroA <- 9
+numeroB <- 4
+print(numeroA - numeroB)
+
+2**3
+base <- 3
+potencia <- 2
+print(base**potencia)
+
+lista <- c(7,8,5,4,3,1)
+plot(lista)
+
+somevar <- readline("Nombre")
+print(somevar)
